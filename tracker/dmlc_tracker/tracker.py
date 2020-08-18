046c8f7ffaed53d8f56321a11522fe76c2628f31
Zhang Zhang <zhang.zhang@intel.com>
2020-08-18 14:32:46 +0800

    for oneccl integration

--- tracker/dmlc_tracker/tracker.py
+++ tracker/dmlc_tracker/tracker.py
@@ -79,60 +79,60 @@ class SlaveEntry(object):
 
     def assign_rank(self, rank, wait_conn, tree_map, parent_map, ring_map):
         self.rank = rank
-        nnset = set(tree_map[rank])
-        rprev, rnext = ring_map[rank]
+        # nnset = set(tree_map[rank])
+        # rprev, rnext = ring_map[rank]
         self.sock.sendint(rank)
-        # send parent rank
-        self.sock.sendint(parent_map[rank])
+        # # send parent rank
+        # self.sock.sendint(parent_map[rank])
         # send world size
         self.sock.sendint(len(tree_map))
-        self.sock.sendint(len(nnset))
-        # send the rprev and next link
-        for r in nnset:
-            self.sock.sendint(r)
-        # send prev link
-        if rprev != -1 and rprev != rank:
-            nnset.add(rprev)
-            self.sock.sendint(rprev)
-        else:
-            self.sock.sendint(-1)
-        # send next link
-        if rnext != -1 and rnext != rank:
-            nnset.add(rnext)
-            self.sock.sendint(rnext)
-        else:
-            self.sock.sendint(-1)
-        while True:
-            ngood = self.sock.recvint()
-            goodset = set([])
-            for _ in range(ngood):
-                goodset.add(self.sock.recvint())
-            assert goodset.issubset(nnset)
-            badset = nnset - goodset
-            conset = []
-            for r in badset:
-                if r in wait_conn:
-                    conset.append(r)
-            self.sock.sendint(len(conset))
-            self.sock.sendint(len(badset) - len(conset))
-            for r in conset:
-                self.sock.sendstr(wait_conn[r].host)
-                self.sock.sendint(wait_conn[r].port)
-                self.sock.sendint(r)
-            nerr = self.sock.recvint()
-            if nerr != 0:
-                continue
-            self.port = self.sock.recvint()
-            rmset = []
-            # all connection was successuly setup
-            for r in conset:
-                wait_conn[r].wait_accept -= 1
-                if wait_conn[r].wait_accept == 0:
-                    rmset.append(r)
-            for r in rmset:
-                wait_conn.pop(r, None)
-            self.wait_accept = len(badset) - len(conset)
-            return rmset
+        # self.sock.sendint(len(nnset))
+        # # send the rprev and next link
+        # for r in nnset:
+        #     self.sock.sendint(r)
+        # # send prev link
+        # if rprev != -1 and rprev != rank:
+        #     nnset.add(rprev)
+        #     self.sock.sendint(rprev)
+        # else:
+        #     self.sock.sendint(-1)
+        # # send next link
+        # if rnext != -1 and rnext != rank:
+        #     nnset.add(rnext)
+        #     self.sock.sendint(rnext)
+        # else:
+        #     self.sock.sendint(-1)
+        # while True:
+        #     ngood = self.sock.recvint()
+        #     goodset = set([])
+        #     for _ in range(ngood):
+        #         goodset.add(self.sock.recvint())
+        #     assert goodset.issubset(nnset)
+        #     badset = nnset - goodset
+        #     conset = []
+        #     for r in badset:
+        #         if r in wait_conn:
+        #             conset.append(r)
+        #     self.sock.sendint(len(conset))
+        #     self.sock.sendint(len(badset) - len(conset))
+        #     for r in conset:
+        #         self.sock.sendstr(wait_conn[r].host)
+        #         self.sock.sendint(wait_conn[r].port)
+        #         self.sock.sendint(r)
+        #     nerr = self.sock.recvint()
+        #     if nerr != 0:
+        #         continue
+        #     self.port = self.sock.recvint()
+        #     rmset = []
+        #     # all connection was successuly setup
+        #     for r in conset:
+        #         wait_conn[r].wait_accept -= 1
+        #         if wait_conn[r].wait_accept == 0:
+        #             rmset.append(r)
+        #     for r in rmset:
+        #         wait_conn.pop(r, None)
+        #     self.wait_accept = len(badset) - len(conset)
+        #     return rmset
 
 class RabitTracker(object):
     """
@@ -302,8 +302,8 @@ class RabitTracker(object):
                         if s.jobid != 'NULL':
                             job_map[s.jobid] = rank
                         s.assign_rank(rank, wait_conn, tree_map, parent_map, ring_map)
-                        if s.wait_accept > 0:
-                            wait_conn[rank] = s
+                        # if s.wait_accept > 0:
+                        #     wait_conn[rank] = s
                         logging.debug('Recieve %s signal from %s; assign rank %d',
                                       s.cmd, s.host, s.rank)
                 if len(todo_nodes) == 0:
